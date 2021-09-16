3dc21748a02d85b0976fc97b9adbfcf0d481360b
xum07 <simon852688@hotmail.com>
2021-09-16 20:10:27 +0800

    update async thread

--- async_thread/main.cpp
+++ async_thread/main.cpp
@@ -15,38 +15,39 @@ void SyncWait()
     std::promise<int> promiseObj;
     std::future<int> futureObj = promiseObj.get_future();
     std::thread t(thread1, &promiseObj);
+    t.detach();
     PRINT_INFO("continue in SyncWait after thread1...\n");
 
     auto result = futureObj.get();
     PRINT_INFO("get result form thread1 in SyncWait: %d\n", result);
-    t.join();
 }
 
-void thread2(std::future<int>* futureObj)
+void thread2(std::future<int>&& futureObj)
 {
     PRINT_INFO("start thread2...\n");
-    auto result = futureObj->get();
+    futureObj.wait();
+    auto result = futureObj.get();
     PRINT_INFO("get result form thread1 in thread2: %d\n", result);
 }
 
+// always failed, because future can not pass to a new thread
 void AsyncWait()
 {
     std::promise<int> promiseObj;
-    std::future<int> futureObj = promiseObj.get_future();
+    std::future<int> futureObj(promiseObj.get_future());
     std::thread t1(thread1, &promiseObj);
+    t1.detach();
     PRINT_INFO("continue in AsyncWait after thread1...\n");
 
-    std::thread t2(thread2, &futureObj);
+    std::thread t2(thread2, std::move(futureObj));
+    t2.detach();
     PRINT_INFO("continue in AsyncWait after thread2...\n");
-    t1.join();
-    t2.join();
 }
-
 
 int main()
 {
     SyncWait();
     AsyncWait();
-
+    std::this_thread::sleep_for(std::chrono::seconds(10));
     return 0;
 }
